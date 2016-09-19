118850bda99b417a718f4d9dfb2e202580f2a0fd
Ryan Brunner <ryan@influitive.com>
2016-09-18 21:23:02 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: "dist",
+        filename: "bundle.js"
+    },
+    module: {
+      loaders: [
+        {
+         test: /.jsx?$/,
+         loader: 'babel-loader',
+         exclude: /node_modules/,
+         query: {
+           presets: ['react']
+         }
+       }
+      ]
+    }
+};
