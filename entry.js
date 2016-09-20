94f0c510e467496304f25f448e8f673ee4fb69f7
Ryan Brunner <ryan@influitive.com>
2016-09-20 10:48:54 -0400

    Included a sample index and entry file

---
+++ entry.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Include your React components like this:
+// var MyComponent = require('components/my_component');
+
+ReactDOM.render(<div>React Element goes here</div>, document.getElementById("placeholder"));
