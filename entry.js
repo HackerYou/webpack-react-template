5ecfe06fc217b2aabc7d6fe11a9ae8974c80125e
Ryan Brunner <ryan@ryanbrunner.com>
2017-01-17 16:29:14 -0500

    Update entry.js

--- entry.js
+++ entry.js
@@ -1,5 +1,5 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Include your React components like this:
 // var MyComponent = require('components/my_component');
