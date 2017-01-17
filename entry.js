da846230312c285fcfcb69dd37a3326d2d6a043b
Ryan Brunner <ryan@ryanbrunner.com>
2017-01-17 16:29:30 -0500

    Update entry.js

--- entry.js
+++ entry.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Include your React components like this:
-// var MyComponent = require('components/my_component');
+// import MyComponent from 'components/my_component';
 
 ReactDOM.render(<div>React Element goes here</div>, document.getElementById("placeholder"));
